d5fe87cd5d9e6ef73e6090783d79ac9efb1e487f
synthylover <125096997+synthylover@users.noreply.github.com>
2023-02-11 01:48:50 +0300

    Update vot.user.js

--- vot.user.js
+++ vot.user.js
@@ -21,7 +21,7 @@
 // @connect          api.browser.yandex.ru
 // ==/UserScript==
 
-const workerHost = "cors.yandexproxy.workers.dev";
+const workerHost = "sparkling-surf-ba6b.giwogek100.workers.dev";
 
 const yandexHmacKey = "gnnde87s24kcuMH8rbWhLyfeuEKDkGGm";
 const yandexUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 CriOS/104.0.5112.114 YaBrowser/22.9.4.633.10 SA/3 Mobile/15E148 Safari/604.1";
